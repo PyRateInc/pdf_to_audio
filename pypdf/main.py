cbc5ac2e9434f309f96302088f3f43b4abb891db
Dimitri Moreira <11929594+Taikamya@users.noreply.github.com>
2020-08-20 17:53:59 -0300

    Add files via upload
    3 bugfixes.
    Added "Play" and "Stop" functions.
    Added debugging "print" functions for faster troubleshooting.
    
    1 known bug:
    App freeze after "Play".

--- pypdf/main.py
+++ pypdf/main.py
@@ -6,56 +6,120 @@ _authors = ["Dimitri", "Pablo III"]
 
 import pyttsx3
 import PyPDF4
-import tkinter as tk
+from tkinter import *
 from tkinter.filedialog import *
 
 
-class App(tk.Frame):
+class App(Frame):
     ''' Cria e mantém uma janela para melhor manuseio, debug e operações comandadas ao invés
         de simplesmente aparecer o prompt para escolha do arquivo sem mais nem menos '''
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.pack()
+        # self.pack()
         self.create_widgets()
         # Estabelece um título
-        self.master.title("PDF Reader")
-        # Estabelece o tamanho MIN da janela principal
-        self.master.minsize(260, 120)
-        # Estabelece o tamanho MAX da janela principal
-        self.master.maxsize(640, 360)
+        root.title("PDF Reader")
+        # Mantém fixo o tamnho da janela
+        root.geometry('{}x{}'.format(360, 240))
+        root.resizable(width=False, height=False)
+        engine.isBusy = False
 
     def create_widgets(self):
-        # Cria um botão "Choose file..." com comando da função "askopenfilename"
-        self.open = tk.Button(self, text="Choose file...", command=self.reading)
-        # self.open["command"] = self.reading
-        self.open.pack(side="top")
+        # Cria uma tela inicial
+        self.open_frame = Frame(root)
+        self.open_frame.pack(side="top")
 
-        # Cria um botão "Quit" com comando da função de fechar a janela principal
-        self.quit = tk.Button(self, text="Quit", fg="yellow", bg="black", command=self.close_window)
-        self.quit.pack(side="bottom")
+        # Cria um botão "Choose file..." com comando da função "extract_txt"
+        self.open_btn = Button(self.open_frame, text="Choose file...", command=self.extract_txt, fg="red", padx=10, pady=10)
+        self.open_btn.grid(row=0, column=0)
+        # Cria um botão "Quit" com comando da função "close_window"
+        self.quit_btn = Button(self.open_frame, text="Quit", command=self.close_window, fg="yellow", bg="black", padx=10, pady=10)
+        self.quit_btn.grid(row=1, column=0)
+
+        # Cria outra tela
+        # self.play_frame = Frame(root)
+        # self.play_frame.pack(side="top")
+
+        # Cria um botão "Play" com comando da função "read_txt"
+        self.play_btn = Button(self.open_frame, text="Play", command=self.read_txt, fg="red", padx=10, pady=10)
+        self.play_btn.grid(row=0, column=1)
+
+        # Cria um botão "Stop" com comando da função de parar a leitura
+        self.stop_btn = Button(self.open_frame, text="Stop", command=self.stop_func, fg="yellow", bg="black", padx=10, pady=10)
+        self.stop_btn.grid(row=1, column=1)
+
+    # def save_state(self):
+    #     pass
+
+    # """ def get_propery(self):
+    #     Pode se colocar as defs rate e a volume aqui e deixá-las como parametros """
+
+    def rate(self):
+        """ Função que determina quantas palavras por minuto, por padrão é 200 palavras por minuto """
+        # Obtém detalhes da taxa atual de fala
+        rate = engine.getProperty('rate')
+        print (rate)
+        engine.setProperty('rate', 100)
+
+    def volume(self):
+        # Reconhece o nível de volume atual (min = 0 e max = 1)
+        volume = engine.getProperty('volume')
+        print (volume)
+        # Configura o nível do volume entre 0 e 1
+        engine.setProperty('volume', 1.0)
 
     def close_window(self):
-        ''' Função pra fechar a janela principal (parent) e qualquer dependente (children). '''
-        Tk.destroy(root)
+        """ Função pra fechar a janela principal (parent) e qualquer dependente (children). """
+        print("Window destroyed.")
+        root.destroy()
 
-    def reading(self):
-        # Colocar um "Try/Except/Else/Finally" aqui, pq se você abre a janela e cancela a operação
-        # O programa não fecha, mas dá erro e não deixa você selecionar depois.
-        # Esse "askopenfilename" é muito bugado
-        book = askopenfilename()
-        pdfreader = PyPDF4.PdfFileReader(book)
-        pages = pdfreader.numPages
+    def extract_txt(self):
+        # RESOLVIDO
+        # RESOLVIDO
+        # RESOLVIDO
+        book = askopenfile(parent=root, mode="rb", title="Choose a PDF file")
+        if book != None:
+            pdfReader = PyPDF4.PdfFileReader(book)
+            global pdfTxt
+            pdfTxt = ""
+            for num in range(pdfReader.numPages):
+                page = pdfReader.getPage(num)
+                pdfTxt += page.extractText()
+            book.close()
+        if pdfReader != None:
+            # Propósito para debug: Ver se o objeto é alocado em memória
+            print(f"Objeto está em {pdfReader}.")
+            print("File is ready.")
+        else:
+            # Propósito para debug: Ver se o objeto NÃO é alocado em memória
+            print(f"Objeto {pdfReader}NÃO está em memória.")
+            print("Operation canceled.")
 
-        for num in range(0, pages):
-            page = pdfreader.getPage(num)
-            text = page.extractText()
-            player = pyttsx3.init()
-            player.say(text)
-            player.runAndWait()
+    def read_txt(self):
+        # Problema! Ainda não consegui fazer parar a leitura, apesar de que a "stop_func" funciona!
+        print("Checking, please wait...")
+        engine.isBusy = True
+        if engine.isBusy == True:
+            engine.say(pdfTxt)
+             # engine.runAndWait()
+            print("Reading has started.")
+        else:
+            print("File is not loaded")
+
+    def stop_func(self):
+        print("Trying to stop it. Please wait...")
+        if engine.isBusy == True:
+            engine.stop()
+            print("Reading stopped.")
+            engine.isBusy = False
+        else:
+            print("Reading was NOT running.")
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    pdfTxt = ""  # Variável global
+    engine = pyttsx3.init()
+    root = Tk()
     myapp = App(master=root)
     myapp.mainloop()
