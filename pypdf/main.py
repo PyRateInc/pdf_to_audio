1ae1c78cbecf1f08b37130b1c9b162982754e9eb
Nemo <nemo@nemo.lan>
2020-08-24 16:17:14 -0400

    O main com o pyttsx está no outro arquivo, esse aqui possui o gtts, e funcionando, testei com os idiomas pt, pt-br e ru(ssia), funciona bem.

--- pypdf/main.py
+++ pypdf/main.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8-*-
+
+''' Feito por: Dimitri(Taikamya) e Pablo III(Nemo) '''
+_authors = ["Dimitri", "Pablo III"]
+
+"""
+    Nota dos autores:
+    Como a ideia é lançar o mais rápido possível, criei uma função que lê em inglês, e outra em português, com o tempo, otimizamos.
+"""
+
 from gtts import gTTS
 import os
 from PyPDF2 import PdfFileReader
 
-def extract_information(pdf_path):
+def extract_information_en(pdf_path):
     with open(pdf_path, 'rb') as f:
         pdf = PdfFileReader(f)
         information = pdf.getDocumentInfo()
@@ -18,14 +29,36 @@ def extract_information(pdf_path):
     Title: {information.title}
     Number of pages: {number_of_pages}
     """
-    tts = gTTS(text=txt, lang='ru')
-    tts.save("book.mp3")
-    os.system("book good.mp3")
+    tts = gTTS(text=txt, lang='en')
+    tts.save("book_en.mp3")
+
+    print(txt)
+    return information
+
+def extract_information_pt_br(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        information = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+
+    txt = f"""
+    Informação sobre {pdf_path}: 
+
+    Autor: {information.author}
+    Criador: {information.creator}
+    Produtor: {information.producer}
+    Assunto: {information.subject}
+    Título: {information.title}
+    Número de páginas: {number_of_pages}
+    """
+    tts = gTTS(text=txt, lang='pt-br')
+    tts.save("book_pt_br.mp3")
 
     print(txt)
     return information
 
 if __name__ == '__main__':
     path = "01OPistoleiroStephenKing.pdf"
-    extract_information(path)
+    extract_information_en(path)
+    extract_information_pt_br(path)
     
